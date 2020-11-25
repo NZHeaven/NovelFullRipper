f89023abf322a866e4229fb44d111890806cf229
josh <josh@fedora33.mshome.net>
2020-11-25 23:10:36 +1300

    Stage one of using Regex to remove script tags

--- V0.1/regextest.py
+++ V0.1/regextest.py
@@ -1,6 +1,23 @@
 #https://stackoverflow.com/questions/11475885/python-replace-regex
+
+#Regex That Worked
+#https://stackoverflow.com/questions/16993795/regex-get-entire-string-between-two-keywords
+#https://stackoverflow.com/questions/159118/how-do-i-match-any-character-across-multiple-lines-in-a-regular-expression/8269712
+#https://regex101.com/r/1NnD9X/4/
+
 import re
+import pdfkit
 
-string = "This is a really <script>Cancer</script> cool script"
+regex = re.compile(r'(<script((.|\n)*?)<\/script>)')
 
-print(re.sub('<script>.*</script>',"",string))
+f = open('test.html','r')
+text = f.read()
+f.close()
+
+edit = re.sub(regex,"",text)
+
+pdfkit.from_string(edit,'test.pdf')
+w = open('test.txt','w')
+w.write(edit)
+w.write("</div>")
+w.close()
